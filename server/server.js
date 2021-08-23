bea00a5101034c5dd989503693bad58771c33461
Zoltanbi <zoltanbiro001@gmail.com>
2021-08-23 13:05:51 -0700

    Created the user signup route

--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ require('./config/db');
 const app = require('express')();
 const port = 3000;
 
-const UserRputer = require('./api/User')
+const UserRouter = require('./api/User')
 
 // FOr accepting post form data
 const bodyParser = require('express').json;
 app.use(bodyParser());
+
+app.use('/user', UserRouter)
 
 app.listen(port, () => {
     console.log(`Server running on ${port}`);
